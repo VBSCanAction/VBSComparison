7dee63be11716fe631d23026bf900aef7ee97af7
Michael Rauch <michael.rauch@kit.edu>
2017-11-16 10:20:47 +0100

    Adjust to new yoda file locations

--- parton-shower/rivetplot.sh
+++ parton-shower/rivetplot.sh
@@ -1,20 +1,20 @@
 #! /bin/sh
 
 rivet-mkhtml \
-  NLO.yoda\
+  VBFNLO/VBFNLO-NLO.yoda\
 :"Title=NLO"\
 :"ErrorBands=1"\
 :"ErrorBandColor=red"\
 :"RatioPlotErrorBandColor=red"\
 :"ErrorBandOpacity=0.3"\
-  Powheg.yoda\
+  Powheg/Powheg.yoda\
 :"Title=Powheg+Pythia 8"\
 :"LineColor=green"\
 :"ErrorBands=1"\
 :"ErrorBandColor=green"\
 :"RatioPlotErrorBandColor=green"\
 :"ErrorBandOpacity=0.3"\
-  Herwig.yoda\
+  VBFNLO/Herwig-MCatNLO-DipoleShower.yoda\
 :"Title=VBFNLO 3+Herwig 7"\
 :"LineColor=brown"\
 :"ErrorBands=1"\
