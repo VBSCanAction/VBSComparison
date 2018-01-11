8394b8cc3b327dbefd03a61fb6f9cd6d3948a8e7
Michael Rauch <michael.rauch@kit.edu>
2018-01-11 10:56:29 +0100

    Update parton-shower plot script and add new plots

--- parton-shower/rivetplot.sh
+++ parton-shower/rivetplot.sh
@@ -1,11 +1,18 @@
 #! /bin/sh
 
-rivet-mkhtml \
+rivet-mkhtml -o rivet-NLOPS \
   VBFNLO/VBFNLO-NLO.yoda\
 :"Title=NLO"\
 :"ErrorBands=1"\
 :"ErrorBandColor=red"\
 :"RatioPlotErrorBandColor=red"\
+:"ErrorBandOpacity=0.3"\
+  VBFNLO/Herwig-MCatNLO-DipoleShower.yoda\
+:"Title=VBFNLO 3+Herwig 7"\
+:"LineColor=blue"\
+:"ErrorBands=1"\
+:"ErrorBandColor=blue"\
+:"RatioPlotErrorBandColor=blue"\
 :"ErrorBandOpacity=0.3"\
   Powheg/Powheg.yoda\
 :"Title=Powheg+Pythia 8"\
@@ -14,24 +21,51 @@ rivet-mkhtml \
 :"ErrorBandColor=green"\
 :"RatioPlotErrorBandColor=green"\
 :"ErrorBandOpacity=0.3"\
-  VBFNLO/Herwig-MCatNLO-DipoleShower.yoda\
-:"Title=VBFNLO 3+Herwig 7"\
+  Powheg/lhef.yoda\
+:"Title=Powheg-no shower"\
+:"LineColor=green"\
+:"LineStyle=dashed"\
+  MG5_aMC/run_HWPP_5M_NLOPS.yoda\
+:"Title=MG5_aMC+Herwig 2.7.1"\
+:"LineColor=magenta"\
+:"ErrorBands=1"\
+:"ErrorBandColor=magenta"\
+:"RatioPlotErrorBandColor=magenta"\
+:"ErrorBandOpacity=0.3"\
+  MG5_aMC/run_PY8_10M_NLOPS.yoda\
+:"Title=MG5_aMC+Pythia 8"\
 :"LineColor=brown"\
 :"ErrorBands=1"\
 :"ErrorBandColor=brown"\
 :"RatioPlotErrorBandColor=brown"\
 :"ErrorBandOpacity=0.3"\
-  Powheg/lhef.yoda\
-:"Title=Powheg-lhef"\
+
+
+rivet-mkhtml -o rivet-LOPS \
+  VBFNLO/VBFNLO-NLO.yoda\
+:"Title=NLO"\
+:"ErrorBands=1"\
+:"ErrorBandColor=red"\
+:"RatioPlotErrorBandColor=red"\
+:"ErrorBandOpacity=0.3"\
+  VBFNLO/Herwig-MCatLO-DipoleShower.yoda\
+:"Title=VBFNLO 3+Herwig 7"\
 :"LineColor=blue"\
 :"ErrorBands=1"\
 :"ErrorBandColor=blue"\
 :"RatioPlotErrorBandColor=blue"\
 :"ErrorBandOpacity=0.3"\
-  MG5_aMC/run_HWPP_5M_NLOPS.yoda\
-:"Title=MG5_aMC-HWPP"\
-:"LineColor=magenta"\
+  MG5_aMC/run_PY8_10M_NLOPS.yoda\
+:"Title=MG5_aMC+Pythia 8"\
+:"LineColor=brown"\
 :"ErrorBands=1"\
-:"ErrorBandColor=magenta"\
-:"RatioPlotErrorBandColor=magenta"\
+:"ErrorBandColor=brown"\
+:"RatioPlotErrorBandColor=brown"\
+:"ErrorBandOpacity=0.3"\
+  PHANTOM/plots_loose_cuts2/phantom_norm.yoda\
+:"Title=PHANTOM+???"\
+:"LineColor=cyan"\
+:"ErrorBands=1"\
+:"ErrorBandColor=cyan"\
+:"RatioPlotErrorBandColor=cyan"\
 :"ErrorBandOpacity=0.3"\
