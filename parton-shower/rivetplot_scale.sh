54532aa0d3f1a429eb6a3795e19fdb4b089f0430
Michael Rauch <michael.rauch@kit.edu>
2018-02-13 15:59:36 +0100

    Add variation script and output
    scale and PDF variation from MG5aMC
    scale variation from VBFNLO

---
+++ parton-shower/rivetplot_scale.sh
@@ -0,0 +1,112 @@
+#! /bin/sh
+
+rivet-mkhtml -o rivet-NLOPSvariation_scale \
+  VBFNLO/VBFNLO-NLO.yoda\
+:"Title=NLO"\
+:"ErrorBands=1"\
+:"ErrorBandColor=red"\
+:"RatioPlotErrorBandColor=red"\
+:"ErrorBandOpacity=0.3"\
+  VBFNLO/Herwig-MCatNLO-DipoleShower_scale.yoda\
+:"Title=VBFNLO 3+H7-Dipole"\
+:"LineColor=blue"\
+:"ErrorBands=1"\
+:"ErrorBandColor=blue"\
+:"RatioPlotErrorBandColor=blue"\
+:"ErrorBandOpacity=0.3"\
+  MG5_aMC/run_PY8_10M_NLOPS.yoda\
+:"Title=MG5_aMC+Pythia 8"\
+:"LineColor=brown"\
+:"ErrorBands=1"\
+:"ErrorBandColor=brown"\
+:"RatioPlotErrorBandColor=brown"\
+:"ErrorBandOpacity=0.3"\
+  MG5_aMC/run_PY8_5M_NLOPS_error_scale.yoda\
+:"Title=MG5_aMC+Pythia 8(scale)"\
+:"LineColor=green"\
+:"ErrorBands=1"\
+:"ErrorBandColor=green"\
+:"RatioPlotErrorBandColor=green"\
+:"ErrorBandOpacity=0.3"\
+
+rivet-mkhtml -o rivet-NLOPSvariation_VBFNLOscale \
+  VBFNLO/VBFNLO-NLO.yoda\
+:"Title=NLO"\
+:"ErrorBands=1"\
+:"ErrorBandColor=red"\
+:"RatioPlotErrorBandColor=red"\
+:"ErrorBandOpacity=0.3"\
+  VBFNLO/Herwig-MCatNLO-DipoleShower_scale.yoda\
+:"Title=VBFNLO 3+H7-Dipole"\
+:"LineColor=blue"\
+:"ErrorBands=1"\
+:"ErrorBandColor=blue"\
+:"RatioPlotErrorBandColor=blue"\
+:"ErrorBandOpacity=0.3"\
+
+rivet-mkhtml -o rivet-NLOPSvariation_MG5aMCscale \
+  VBFNLO/VBFNLO-NLO.yoda\
+:"Title=NLO"\
+:"ErrorBands=1"\
+:"ErrorBandColor=red"\
+:"RatioPlotErrorBandColor=red"\
+:"ErrorBandOpacity=0.3"\
+  MG5_aMC/run_PY8_10M_NLOPS.yoda\
+:"Title=MG5_aMC+Pythia 8"\
+:"LineColor=brown"\
+:"ErrorBands=1"\
+:"ErrorBandColor=brown"\
+:"RatioPlotErrorBandColor=brown"\
+:"ErrorBandOpacity=0.3"\
+  MG5_aMC/run_PY8_5M_NLOPS_error_scale.yoda\
+:"Title=MG5_aMC+Pythia 8(scale)"\
+:"LineColor=green"\
+:"ErrorBands=1"\
+:"ErrorBandColor=green"\
+:"RatioPlotErrorBandColor=green"\
+:"ErrorBandOpacity=0.3"\
+
+rivet-mkhtml -o rivet-NLOPSvariation_MG5aMCPDF \
+  VBFNLO/VBFNLO-NLO.yoda\
+:"Title=NLO"\
+:"ErrorBands=1"\
+:"ErrorBandColor=red"\
+:"RatioPlotErrorBandColor=red"\
+:"ErrorBandOpacity=0.3"\
+  MG5_aMC/run_PY8_10M_NLOPS.yoda\
+:"Title=MG5_aMC+Pythia 8"\
+:"LineColor=brown"\
+:"ErrorBands=1"\
+:"ErrorBandColor=brown"\
+:"RatioPlotErrorBandColor=brown"\
+:"ErrorBandOpacity=0.3"\
+  MG5_aMC/run_PY8_5M_NLOPS_error_pdf.yoda\
+:"Title=MG5_aMC+Pythia 8(PDF)"\
+:"LineColor=green"\
+:"ErrorBands=1"\
+:"ErrorBandColor=green"\
+:"RatioPlotErrorBandColor=green"\
+:"ErrorBandOpacity=0.3"\
+
+rivet-mkhtml -o rivet-NLOPSvariation_MG5aMCboth \
+  VBFNLO/VBFNLO-NLO.yoda\
+:"Title=NLO"\
+:"ErrorBands=1"\
+:"ErrorBandColor=red"\
+:"RatioPlotErrorBandColor=red"\
+:"ErrorBandOpacity=0.3"\
+  MG5_aMC/run_PY8_10M_NLOPS.yoda\
+:"Title=MG5_aMC+Pythia 8"\
+:"LineColor=brown"\
+:"ErrorBands=1"\
+:"ErrorBandColor=brown"\
+:"RatioPlotErrorBandColor=brown"\
+:"ErrorBandOpacity=0.3"\
+  MG5_aMC/run_PY8_5M_NLOPS_error_all.yoda\
+:"Title=MG5_aMC+Pythia 8(both)"\
+:"LineColor=green"\
+:"ErrorBands=1"\
+:"ErrorBandColor=green"\
+:"RatioPlotErrorBandColor=green"\
+:"ErrorBandOpacity=0.3"\
+
