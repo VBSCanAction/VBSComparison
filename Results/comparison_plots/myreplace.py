9aa81edabeab76daab94093b72e217d89841ffa5
pstienem <pascal.stienemeier@desy.de>
2018-02-24 19:47:48 +0100

    Gnuplot-plots: 	Remove "MonteCarlo comparison" and "fixed order" from subcaption

--- Results/comparison_plots/myreplace.py
+++ Results/comparison_plots/myreplace.py
@@ -13,8 +13,10 @@ content = open(infile).read()
 #                          'set terminal pdf font "Helvetica,12" enhanced dashed size 8 cm, 12 cm')
 #content = content.replace('set label "e^+mu^+{/Symbol nn}jj production at the LHC, 13 TeV" font ",14" at graph 0.1, graph 0.9', \
 #                          'set label "e^+mu^+{/Symbol nn}jj production at the LHC, 13 TeV" font ",12" at graph 0.03, graph 0.9')
-content = content.replace('set label "MonteCarlo comparison, NLO fixed order" font ",12" at graph 0.1, graph 0.88', \
-                          'set label "MonteCarlo comparison, NLO fixed order" font ",10" at graph 0.03, graph 0.88')
+#content = content.replace('set label "MonteCarlo comparison, NLO fixed order" font ",12" at graph 0.1, graph 0.88', \
+#                          'set label "MonteCarlo comparison, NLO fixed order" font ",10" at graph 0.03, graph 0.88')
+content = content.replace('set label "MonteCarlo comparison, NLO fixed order" font ",10" at graph 0.03, graph 0.88', \
+                          'set label "NLO" font ",10" at graph 0.03, graph 0.88')
 #content = content.replace('set key at graph 1, graph 0.9 noautotitles spacing 2.4', \
 #                          'set key at graph 1, graph 0.8 noautotitles spacing 2.4')
 #content = content.replace('set ylabel \'Ratio /VBFNLO\'', \
--- Results/comparison_plots/plot_dyj1j2_NLO.gnuplot
+++ Results/comparison_plots/plot_dyj1j2_NLO.gnuplot
@@ -51,7 +51,7 @@ BONSAY_fact=1e-3 #numbers are in fb/GeV
 PHANTOM_fact=1e-3 #numbers are in fb/GeV
 
 set label "e^+mu^+{/Symbol nn}jj production at the LHC, 13 TeV" font ",12" at graph 0.03, graph 0.94
-set label "MonteCarlo comparison, NLO fixed order" font ",10" at graph 0.03, graph 0.88
+set label "NLO" font ",10" at graph 0.03, graph 0.88
 set xrange [0:10]
 set yrange [1e-6:1e-3]
 set logscale y
